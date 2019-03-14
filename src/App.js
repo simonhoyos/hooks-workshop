b7bc75b7012a15e7549b10c840fe12818f3007f8
Simon Hoyos <shmesa23@gmail.com>
2019-03-14 18:30:49 -0500

    Basic components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import { Navigation } from "./components/Navigation";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>Hello Hooks</p>
+        <Navigation />
+        <Navigation />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// Workshop
+import App from './App';
+// Example
+// import { Counter } from "./example/Counter";
+
+// Workshop
 ReactDOM.render(<App />, document.getElementById('root'));
+// Example
+// ReactDOM.render(<Counter />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
