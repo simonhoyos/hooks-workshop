6ee136e9b17465c4c6b47f62674aa4e2d14b6a63
Simon Hoyos <shmesa23@gmail.com>
2019-03-20 11:13:06 -0500

    Step 3 example

--- src/example/Timer.js
+++ src/example/Timer.js
@@ -1,27 +1,153 @@
 import React, { Component } from 'react';
 import './Timer.css';
 
+function formatTime(time) {
+  return time / 10 >= 1 ? time : `0${time}`;
+}
+
+function TimeBox({ time, name, isRunning, handleDecrement, handleIncrement }) {
+  const maxTime = name === 'hours' ? 23 : 59;
+  return (
+    <div>
+      <h2>{name}</h2>
+      <p>{formatTime(time)}</p>
+      <button
+        name={name}
+        onClick={handleDecrement}
+        disabled={time <= 0 || isRunning}
+      >
+        -
+      </button>
+      <button
+        name={name}
+        onClick={handleIncrement}
+        disabled={time >= maxTime || isRunning}
+      >
+        +
+      </button>
+    </div>
+  )
+}
+
 class Timer extends Component {
   state = {
-    time: '',
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+    isRunning: false,
   };
 
-  componentDidMount() {
-    this.clock = setInterval(() => {
-      this.setState({
-        time: new Date().toLocaleTimeString(),
-      })
+  handleDecrement = e => {
+    const name = e.target.name;
+
+    this.setState(prevState => ({
+      [name]: prevState[name] - 1,
+    }));
+  }
+
+  handleIncrement = e => {
+    const name = e.target.name;
+
+    this.setState(prevState => ({
+      [name]: prevState[name] + 1,
+    }));
+  }
+
+  handleStart = () => {
+    this.setState({ isRunning: true });
+
+    this.timer = setInterval(() => {
+      const { hours, minutes, seconds } = this.state;
+
+      if (hours > 0 && (minutes <= 0 && seconds <= 0)) {
+        this.setState(({ hours }) => ({
+          hours: hours - 1,
+          minutes: 59,
+          seconds: 59,
+        }));
+        return;
+      }
+
+      if (minutes > 0 && seconds <= 0) {
+        this.setState(({ minutes }) => ({
+          minutes: minutes - 1,
+          seconds: 59,
+        }));
+        return;
+      }
+
+      this.setState(({ seconds }) => ({
+        seconds: seconds - 1,
+      }), () => {
+        if (hours <= 0 && minutes <= 0 && seconds - 1 <= 0) {
+          this.handleStop();
+          return;
+        }
+      });
+      return;
     }, 1000);
   }
 
-  componentWillUnmount() {
-    clearInterval(this.clock);
+  handleStop = () => {
+    this.setState({ isRunning: false });
+    clearInterval(this.timer);
+  }
+
+  handleReset = () => {
+    this.setState({
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+      isRunnig: false,
+    });
   }
 
   render() {
+    const { hours, minutes, seconds, isRunning } = this.state;
     return (
-      <div>
-        <p>{this.state.time}</p>
+      <div className="container">
+        <TimeBox
+          name="hours"
+          time={hours}
+          handleDecrement={this.handleDecrement}
+          handleIncrement={this.handleIncrement}
+          isRunning={isRunning}
+        />
+        <TimeBox
+          name="minutes"
+          time={minutes}
+          handleDecrement={this.handleDecrement}
+          handleIncrement={this.handleIncrement}
+          isRunning={isRunning}
+        />
+        <TimeBox
+          name="seconds"
+          time={seconds}
+          handleDecrement={this.handleDecrement}
+          handleIncrement={this.handleIncrement}
+          isRunning={isRunning}
+        />
+        <button
+          onClick={this.handleStart}
+          className="start"
+          disabled={(hours <= 0 && minutes <= 0 && seconds <= 0) || isRunning}
+        >
+          Start
+        </button>
+        <button
+          onClick={this.handleStop}
+          className="stop"
+          disabled={!isRunning}
+        >
+          Stop
+        </button>
+        <button
+          onClick={this.handleReset}
+          className="reset"
+          disabled={isRunning}
+        >
+          Reset
+        </button>
       </div>
     )
   }
--- src/example/solution.js
+++ src/example/solution.js
@@ -1,25 +1,169 @@
-import React, { useState, useEffect } from 'react';
+import React, { useReducer } from 'react';
 import './Timer.css';
 
-export function Timer() {
-  const [count, setCount] = useState(0);
-  const [time, setTime] = useState(new Date().toLocaleTimeString());
+function formatTime(time) {
+  return time / 10 >= 1 ? time : `0${time}`;
+}
 
-  useEffect(() => {
-    setCount(prevCount => prevCount + 1);
-    let clock;
-    clock = setInterval(() => {
-      setTime(new Date().toLocaleTimeString());
-    });
-    return () => {
-      clearInterval(clock);
-    }
-  }, [time]);
+function init(initialState) {
+  return initialState;
+}
 
+function tick({ hours, minutes, seconds }) {
+  if (hours > 0 && (minutes <= 0 && seconds <= 0)) {
+    return { hours: hours - 1, minutes: 59, seconds: 59};
+  }
+
+  if (minutes > 0 && seconds <= 0) {
+    return { minutes: minutes - 1, seconds: 59 };
+  }
+
+  return { seconds: seconds - 1 };
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'decrement':
+      return {
+        ...state,
+        [action.payload.name]: state[action.payload.name] - 1,
+      };
+    case 'increment':
+      return {
+        ...state,
+        [action.payload.name]: state[action.payload.name] + 1,
+      };
+    case 'start':
+      return {
+        ...state,
+        isRunning: true,
+      };
+    case 'stop':
+      return {
+        ...state,
+        isRunning: false,
+      };
+    case 'tick':
+      const payload = tick(state);
+      return {
+        ...state,
+        ...payload,
+      };
+    case 'reset':
+      return init(action.payload);
+    default:
+      return state;
+  }
+}
+
+const initialState = {
+  hours: 0,
+  minutes: 0,
+  seconds: 0,
+  isRunning: false,
+};
+
+function Timer() {
+  const [state, dispatch] = useReducer(reducer, initialState, init);
+  let timer;
+
+  function handleDecrement(e) {
+    dispatch({ type: 'decrement', payload: { name: e.target.name } });
+  }
+
+  function handleIncrement(e) {
+    dispatch({ type: 'increment', payload: { name: e.target.name } });
+  }
+
+  function handleStart() {
+    dispatch({ type: 'start' });
+    timer = setInterval(() => dispatch({ type: 'tick' }), 1000);
+  }
+
+  function handleStop() {
+    dispatch({ type: 'stop' });
+    clearInterval(timer);
+  }
+
+  function handleReset() {
+    dispatch({ type: 'reset', payload: initialState });
+  }
+
+  if (state.isRunning && state.hours <= 0 && state.minutes <= 0 && state.seconds <= 0) {
+    handleStop();
+  }
+
+  const { hours, minutes, seconds, isRunning } = state;
   return (
-    <div>
-      <p>{count}</p>
-      <p>{time}</p>
+    <div className="container">
+      <TimeBox
+        name="hours"
+        time={hours}
+        handleDecrement={handleDecrement}
+        handleIncrement={handleIncrement}
+        isRunning={isRunning}
+      />
+      <TimeBox
+        name="minutes"
+        time={minutes}
+        handleDecrement={handleDecrement}
+        handleIncrement={handleIncrement}
+        isRunning={isRunning}
+      />
+      <TimeBox
+        name="seconds"
+        time={seconds}
+        handleDecrement={handleDecrement}
+        handleIncrement={handleIncrement}
+        isRunning={isRunning}
+      />
+      <button
+        onClick={handleStart}
+        className="start"
+        disabled={(hours <= 0 && minutes <= 0 && seconds <= 0) || isRunning}
+      >
+        Start
+      </button>
+      <button
+        onClick={handleStop}
+        className="stop"
+        disabled={!isRunning}
+      >
+        Stop
+      </button>
+      <button
+        onClick={handleReset}
+        className="reset"
+        disabled={isRunning}
+      >
+        Reset
+      </button>
     </div>
   )
 }
+
+function TimeBox({ time, name, isRunning, handleDecrement, handleIncrement }) {
+  const maxTime = name === 'hours' ? 23 : 59;
+  return (
+    <div>
+      <h2>{name}</h2>
+      <p>{formatTime(time)}</p>
+      <button
+        name={name}
+        onClick={handleDecrement}
+        disabled={time <= 0 || isRunning}
+      >
+        -
+      </button>
+      <button
+        name={name}
+        onClick={handleIncrement}
+        disabled={time >= maxTime || isRunning}
+      >
+        +
+      </button>
+    </div>
+  )
+}
+
+export { Timer };
