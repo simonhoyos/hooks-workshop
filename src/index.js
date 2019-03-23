760b8e1bd94a0ac9baadd3ed62a1f55cbcb335c0
Simon Hoyos <shmesa23@gmail.com>
2019-03-23 18:58:31 -0500

    Step 4 add context provider

--- src/index.js
+++ src/index.js
@@ -5,13 +5,19 @@ import * as serviceWorker from './serviceWorker';
 
 // Workshop
 import App from './App';
+import { PaginationProvider } from './store';
 // Example
-// import { Timer } from "./example/Timer";
+// import { Text } from "./example/Text";
 
 // Workshop
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <PaginationProvider>
+    <App />
+  </PaginationProvider>,
+  document.getElementById('root')
+);
 // Example
-// ReactDOM.render(<Timer />, document.getElementById('root'));
+// ReactDOM.render(<Text />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
